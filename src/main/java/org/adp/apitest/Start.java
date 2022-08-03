b1780e1d77e64e223ae81dfb2390261e0d511959
BreakZhao <1694392889@qq.com>
2022-08-03 08:46:37 +0800

    boot with spring

---
+++ src/main/java/org/adp/apitest/Start.java
@@ -0,0 +1,16 @@
+package org.adp.apitest;
+
+import lombok.Generated;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zzq
+ */
+@SpringBootApplication
+public class Start {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Start.class, args);
+    }
+}
