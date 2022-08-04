d9332565d05155fe8faa2b091e811debc048e597
BreakZhao <1694392889@qq.com>
2022-08-04 15:36:40 +0800

    add get json api

--- src/main/java/org/adp/apitest/controller/GetTestController.java
+++ src/main/java/org/adp/apitest/controller/GetTestController.java
@@ -1,10 +1,15 @@
 package org.adp.apitest.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
 
 /**
  * @author zzq
@@ -13,9 +18,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("get")
 public class GetTestController {
 
+    @Resource
+    private ObjectMapper objectMapper;
+
     @GetMapping("str")
     @ResponseBody
     public String getStr() {
         return RandomStringUtils.random(20, true, false);
+    }
+
+    @GetMapping("json")
+    @ResponseBody
+    public ObjectNode getStr(@RequestParam String name, @RequestParam(name = "str", required = false) String str) {
+        ObjectNode resource = objectMapper.createObjectNode()
+                .put("name", name)
+                .put("salt", RandomStringUtils.random(15, true, false))
+                .put("str", str);
+        return resource;
     }
 }
