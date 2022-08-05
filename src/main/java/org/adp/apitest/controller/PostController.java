141ad6b7d94cd1d45e60ed5548b5e23cb42a06f4
BreakZhao <1694392889@qq.com>
2022-08-05 14:27:48 +0800

    add post http api

---
+++ src/main/java/org/adp/apitest/controller/PostController.java
@@ -0,0 +1,38 @@
+package org.adp.apitest.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+/**
+ * @author zzq
+ */
+@Controller
+@RequestMapping("post")
+public class PostController {
+
+    @Resource
+    private ObjectMapper objectMapper;
+
+    @PostMapping("withQuery")
+    @ResponseBody
+    public String returnStr(@RequestParam String name) {
+        return RandomStringUtils.random(20, true, false) + "_" + name;
+    }
+
+    @PostMapping("json")
+    @ResponseBody
+    public ObjectNode returnStr(@RequestParam String name, @RequestParam(required = false) String str) {
+        return objectMapper
+                .createObjectNode()
+                .put("name", name)
+                .put("str", str)
+                .put("random", RandomStringUtils.random(10, true, false))
+                ;
+    }
+
+}
