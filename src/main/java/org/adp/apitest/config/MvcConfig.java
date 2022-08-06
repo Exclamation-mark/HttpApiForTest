009cc47e23d6188fa14e713ca81d82cff822d486
zzq <1694392889@qq.com>
2022-08-06 11:37:01 +0800

    cors config

---
+++ src/main/java/org/adp/apitest/config/MvcConfig.java
@@ -0,0 +1,35 @@
+package org.adp.apitest.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author zzq
+ */
+@Configuration
+@Slf4j
+public class MvcConfig implements WebMvcConfigurer {
+
+    /**
+     * cors config of urls‘s mapping
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS")
+                .allowCredentials(true).maxAge(3600);
+    }
+
+}
